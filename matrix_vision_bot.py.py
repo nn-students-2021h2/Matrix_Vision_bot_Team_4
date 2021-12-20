738d2a251bb7f6d5625aa72f192888960581cac8
Aniloren <enderxoma2014@yandex.ru>
2021-12-20 18:58:19 +0300

    Adding modified echo simple telegram bot

--- matrix_vision_bot.py.py
+++ matrix_vision_bot.py.py
@@ -1,38 +1,71 @@
-import requests
-import json
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
 
-TOKEN = 'YOUR_TOKEN' # insert your token from BotFather here
-BASE_URL = f"https://api.telegram.org/bot{TOKEN}/"
-LONG_POLLING_TIMEOUT = 10
-
-def bot_function():
-    pass
+import logging
 
 
-def main(PLACEHOLDER_MSG="Привет", any_arguments) -> None:
-    last_update_id = None
+from telegram import Bot, Update
+from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler, Updater
 
-    while True:
-        r = requests.get(
-            BASE_URL + "getUpdates",
-            params={"offset": last_update_id, "timeout": LONG_POLLING_TIMEOUT},
-        )
-        response_dict = json.loads(r.text)
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-        print(r.status_code)
-        print(r.text)
+logger = logging.getLogger(__name__)
 
-        for upd in response_dict["result"]:
-            last_update_id = upd["update_id"] + 1
-            msg = upd["message"]
-            chat_id = msg["chat"]["id"]
-
-            if "text" in msg:
-                r = requests.post(
-                    BASE_URL + "sendMessage",
-                    params={"chat_id": chat_id, "text": PLACEHOLDER_MSG},
-                )
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
 
 
-if __name__ == "__main__":
+def start(update: Update, context: CallbackContext):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text(f'Привет, {update.effective_user.first_name}!')
+
+
+def chat_help(update: Update, context: CallbackContext):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Введи команду /start для начала. ')
+
+
+def echo(update: Update, context: CallbackContext):
+    """Echo the user message."""
+    #update.message.reply_text(update.message.text)
+    update.message.reply_text("!!!")
+
+
+def error(update: Update, context: CallbackContext):
+    """Log Errors caused by Updates."""
+    logger.warning(f'Update {update} caused error {context.error}')
+
+
+def main():
+    f = open('TOKEN.txt')
+    TOKEN = f.read()
+
+    bot = Bot(
+        token=TOKEN,
+    )
+    updater = Updater(bot=bot, use_context=True)
+
+    # on different commands - answer in Telegram
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CommandHandler('help', chat_help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    updater.dispatcher.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    logger.info('Start Bot')
     main()
